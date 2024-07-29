f4d609999900c73ef208bdbc4c8038bac685fcaf
hackout <hackout@vip.qq.com>
2024-07-29 16:02:32 +0800

    seeder fixed

--- database/seeders/CompanySeeder.php
+++ database/seeders/CompanySeeder.php
@@ -15,123 +15,52 @@ class CompanySeeder extends Seeder
         $data = $this->getList();
         foreach ($data as $sql) {
             if (!Dict::where('code', $sql['code'])->first()) {
-                if ($dict = Dict::create(['name' => $sql['name'], 'code' => $sql['code']])) {
-                    $dict->items()->createMany($sql['children']);
-                }
+                $this->addDict($sql);
             }
         }
     }
 
-    public function getList()
+    private function addDict($data): void
+    {
+        $dict = Dict::create(['name' => $data['name'], 'code' => $data['code']]);
+        $dict->items()->createMany($this->convertChildren($data['children']));
+
+    }
+
+    private function convertChildren(array $data): array
+    {
+        $result = [];
+        foreach ($data as $value => $name) {
+            $result[] = [
+                'name' => $name,
+                'content' => $value
+            ];
+        }
+        return $result;
+    }
+
+    private function getList()
     {
         return [
             [
                 'name' => '企业状态',
                 'code' => 'company_status',
-                'children' => [
-                    [
-                        'name' => '营业中',
-                        'content' => 0,
-                    ],
-                    [
-                        'name' => '歇业',
-                        'content' => 1,
-                    ],
-                    [
-                        'name' => '闭业',
-                        'content' => 2,
-                    ],
-                    [
-                        'name' => '清退',
-                        'content' => 3,
-                    ]
-                ]
+                'children' => ['营业中', '歇业', '闭业', '清退']
             ],
             [
                 'name' => '企业申请状态',
                 'code' => 'company_apply_status',
-                'children' => [
-                    [
-                        'name' => '待提交',
-                        'content' => 0,
-                    ],
-                    [
-                        'name' => '审核中',
-                        'content' => 1,
-                    ],
-                    [
-                        'name' => '审核通过',
-                        'content' => 2,
-                    ],
-                    [
-                        'name' => '审核拒绝',
-                        'content' => 3,
-                    ],
-                    [
-                        'name' => '关闭',
-                        'content' => 4,
-                    ]
-                ]
+                'children' => ['待提交', '审核中', '审核通过', '审核拒绝', '关闭']
             ],
             [
                 'name' => '企业日志请求类型',
                 'code' => 'company_log_method',
-                'children' => [
-                    [
-                        'name' => 'OPTION',
-                        'content' => 0,
-                    ],
-                    [
-                        'name' => 'GET',
-                        'content' => 1,
-                    ],
-                    [
-                        'name' => 'POST',
-                        'content' => 2,
-                    ],
-                    [
-                        'name' => 'PUT',
-                        'content' => 3,
-                    ],
-                    [
-                        'name' => 'PATCH',
-                        'content' => 4,
-                    ],
-                    [
-                        'name' => 'DELETE',
-                        'content' => 5,
-                    ]
-                ]
+                'children' => ['OPTION', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE']
             ],
             [
                 'name' => '企业等级标识',
                 'code' => 'company_level',
-                'children' => [
-                    [
-                        'name' => 'Lv.0',
-                        'content' => 0,
-                    ],
-                    [
-                        'name' => 'Lv.1',
-                        'content' => 1,
-                    ],
-                    [
-                        'name' => 'Lv.2',
-                        'content' => 2,
-                    ],
-                    [
-                        'name' => 'Lv.3',
-                        'content' => 3,
-                    ],
-                    [
-                        'name' => 'Lv.4',
-                        'content' => 4,
-                    ],
-                    [
-                        'name' => 'Lv.5',
-                        'content' => 5,
-                    ]
-                ]
+                'children' => ['Lv.0', 'Lv.1', 'Lv.2', 'Lv.3', 'Lv.4', 'Lv.5']
             ],
         ];
     }
